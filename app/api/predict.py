04002ca08927dbfd7393863475603586dff70195
Navroz Lamba <lamba-navroz@lambdastudents.com>
2020-09-21 15:38:02 +0530

    Deploy to Heroku

--- app/api/predict.py
+++ app/api/predict.py
@@ -3,7 +3,7 @@ import random
 
 from fastapi import APIRouter
 import pandas as pd
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field
 
 log = logging.getLogger(__name__)
 router = APIRouter()
