d53ebff10bd5a09ed05a75cafbfef269035534b9
Navroz Lamba <lamba-navroz@lambdastudents.com>
2020-09-23 18:57:53 +0530

    Added the ML model

--- app/api/predict.py
+++ app/api/predict.py
@@ -1,9 +1,8 @@
-import logging
-import random
-import json
+import logging, random, json, joblib, sklearn
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field
+from typing import Dict
 
 log = logging.getLogger(__name__)
 router = APIRouter()
@@ -11,41 +10,29 @@ router = APIRouter()
 strains = pd.read_csv('data/strains.csv')
 list_of_strains = strains.name
 
+predictor = joblib.load('app/api/predictor.joblib')
+print('pickled model loaded')
+
 class Strain(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    kind: str = Field(..., example='Hybrid')
     ailment: str = Field(..., example='Insomnia')
+    kind: str = Field(..., example='Hybrid')
     effect: str = Field(..., example='Happy')
     flavor: str = Field(..., example='Blueberry')
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
-        return pd.DataFrame(self)
+        return pd.DataFrame([dict(self)])
 
 
 @router.post('/predict')
 async def predict(strain: Strain):
-    """
-    Make random baseline prediction for classification problem 🔮
-
-    ### Request Body
-   
-    - `kind`: str
-    - `ailment`: str
-    - `effect`: str
-     -`flavor`: str
-
-    ### Response
-    - `prediction`: string, at random
-    """
-
-    # X_new = strain.to_df()
-    # log.info(X_new)
-
-    y_pred = random.choice(list_of_strains)
+    
+    predict_strain = predictor.predict(strain.to_df())[0]
+    
     # making a new df for our predicted value 
-    y_pred_df = strains.loc[strains['name'] == y_pred]
+    y_pred_df = strains.loc[strains['name'] == predict_strain]
 
     # description = y_pred_df.Description.to_string(index=False)
     description = y_pred_df.Description.to_json(orient='values')
@@ -53,7 +40,7 @@ async def predict(strain: Strain):
     
 
     return {
-        'prediction': y_pred,
+        'prediction': predict_strain,
         'description': description,
         'rating': rating
     }
